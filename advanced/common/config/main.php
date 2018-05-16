1aec6011ab49e7479e7ad5b8400aa2553b780a6b
lijianlin <lijianlin@edaijia-inc.cn>
2018-05-16 18:24:14 +0800

    gii增加后台user模块

--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -9,5 +9,12 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=personal',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8',
+        ],
     ],
 ];
